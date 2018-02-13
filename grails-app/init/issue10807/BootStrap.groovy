bcb666a65f858e6cbc6b72cef9fd3eb9b8816bf6
Iván López <lopez.ivan@gmail.com>
2018-02-13 09:53:51 +0100

    Define logback files per environment

--- grails-app/init/issue10807/BootStrap.groovy
+++ grails-app/init/issue10807/BootStrap.groovy
@@ -2,7 +2,10 @@ package issue10807
 
 class BootStrap {
 
+    def grailsApplication
+
     def init = { servletContext ->
+        println grailsApplication.config.getProperty('logging.config')
     }
     def destroy = {
     }
